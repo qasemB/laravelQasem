2c072a1d58892e79082b8102c52b7f51c28c971a
qasem <74683761+qasemB@users.noreply.github.com>
2020-11-23 13:39:27 +0330

    delete tags that i forget

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -36,13 +36,6 @@ class ShopController extends Controller
      */
     public function store(StoreShope $request)
     {
-        // $request->validate([
-        //     'title'=> 'required|between:8,50|regex:/^[ا-یa-zA-Z0-9\-۰−۹ي−،., ]+$/u',
-        //     'description'=> 'required|max:2000|regex:/^[ا-یa-zA-Z0-9\-۰−۹ي−،., ]+$/u',
-        //     'price'=> 'required|numeric|min:10',
-        //     'image'=> 'required|mimes:png,jpg,jpeg|between:50,5000'
-        // ]);
-
         Shop::create($request->all());
 
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
